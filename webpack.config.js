58fd843f4070604820e98f894508b86eb1f4c49d
Zoë Bryant <zoebryant@Zoes-MacBook-Pro.local>
2016-04-28 15:33:44 +0100

    Hot reloading of React classes individually

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const TARGET = process.env.npm_lifecycle_event;
+
+process.env.BABEL_ENV = TARGET;
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
