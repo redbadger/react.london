8f1a12a2ffa2e0b91432cb93e87db3801d641d75
Marcel Cutts <marcel.cutts@red-badger.com>
2016-04-29 11:58:53 +0100

    added example tests for application boilerplate using enzyme + mocha

--- app/components/Preview/Preview-test.js
+++ app/components/Preview/Preview-test.js
@@ -4,11 +4,17 @@ import { shallow, mount, render } from 'enzyme';
 
 import Preview from './Preview.js';
 
-const exampleUserTexts = [{id: 1, value:"hello"}];
+const exampleUserTexts = [{ id: 1, value: 'hello' }];
 
 describe('Preview view', function () {
   it('contains a span with className special', function () {
-    let renderedComponent = shallow(<Preview userTexts={exampleUserTexts} />)
-    expect(renderedComponent.contains(<span className="special">React</span>)).to.equal(true);
+    let renderedComponent = shallow(<Preview userTexts={exampleUserTexts} />);
+    expect(renderedComponent.find('.Preview__heading--special')).to.have.length(1);
+  });
+
+  it('renders user texts correctly', function () {
+    let renderedComponent = shallow(<Preview userTexts={exampleUserTexts} />);
+    let userText = renderedComponent.find('.userText').text();
+    expect(userText).to.equal('User said: hello');
   });
 });
--- app/components/Preview/Preview.js
+++ app/components/Preview/Preview.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 import style from './Preview.css';
 
-const Preview = ({userTexts}) => (
+const Preview = ({ userTexts }) => (
     <div>
       <h1>This is <span className='Preview__heading--special'>React!</span></h1>
 
-      {userTexts.map(u => (<div key={u.id}>User said: {u.value}</div>))}
+      {userTexts.map(u => (<div className='userText' key={u.id}>User said: {u.value}</div>))}
     </div>
   );
 
