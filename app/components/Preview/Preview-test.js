1721d52090f7f4d703a1baba77d01f27ed76f1e9
Marcel Cutts <marcel.cutts@red-badger.com>
2016-04-29 12:00:39 +0100

    Es2015 syntax for tests

--- app/components/Preview/Preview-test.js
+++ app/components/Preview/Preview-test.js
@@ -6,13 +6,13 @@ import Preview from './Preview.js';
 
 const exampleUserTexts = [{ id: 1, value: 'hello' }];
 
-describe('Preview view', function () {
-  it('contains a span with className special', function () {
+describe('Preview view', () => {
+  it('contains a span with className special', () => {
     let renderedComponent = shallow(<Preview userTexts={exampleUserTexts} />);
     expect(renderedComponent.find('.Preview__heading--special')).to.have.length(1);
   });
 
-  it('renders user texts correctly', function () {
+  it('renders user texts correctly', () => {
     let renderedComponent = shallow(<Preview userTexts={exampleUserTexts} />);
     let userText = renderedComponent.find('.userText').text();
     expect(userText).to.equal('User said: hello');
