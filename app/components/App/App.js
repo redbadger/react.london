2cee92d501d4246c5d3201dda005c8fba3cfcbf5
Zoë Bryant <zoebryant@Zoes-MacBook-Pro.local>
2016-05-13 16:06:51 +0100

    Refactored out App component

---
+++ app/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Editor from '../../containers/Editor';
+import Preview from '../../containers/Preview';
+
+import Radium, { Style } from 'radium';
+
+const App = Radium(({ children }) => (
+  <div style={styles}>
+    <Editor />
+    <Preview />
+  </div>
+));
+
+const styles = {
+  width: '100%',
+  display: 'flex',
+};
+
+export default App;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute, Link } from 'react-router';
 
-import Editor from '../containers/Editor';
-import Preview from '../containers/Preview';
-
-import Radium, { Style } from 'radium';
-
-const App = Radium(({ children }) => (
-  <div style={styles}>
-    <Editor />
-    <Preview />
-  </div>
-));
+import App from '../components/App/App';
 
 const routes = (
   <Route path="/" component={App} />
 );
-
-const styles = {
-  width: '100%',
-  display: 'flex',
-};
 
 export default routes;
