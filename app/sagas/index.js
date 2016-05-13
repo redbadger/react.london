47ee7f78cd14ca764214b3b6136468e0a7495d5c
Zoë Bryant <zoebryant@Zoes-MacBook-Pro.local>
2016-05-13 14:49:33 +0100

    Maps the data to the fields

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,13 +1,18 @@
 import { makeFetch } from '../api';
 import { takeLatest } from 'redux-saga'
 import { call, put } from 'redux-saga/effects';
+import { initialize } from 'redux-form';
+
 
 export function* fetchContent() {
   try {
     yield put({ type: 'FETCHING_CONTENT' });
     // TODO: remove hardcoded localhost url
+    
+    //
     const content = yield call(makeFetch, 'http://localhost:8080/content');
-    yield put({ type: 'FETCHED_CONTENT', payload: content });
+    //yield put({ type: 'FETCHED_CONTENT', payload: content });
+    yield put(initialize('editor', content));
   } catch (e) {
     yield put({ type: 'CONTENT_FETCH_FAILED', message: e.message });
   }
