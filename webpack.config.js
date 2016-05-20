fecedacd86e4472d9a67a693cd3fe952ebdb94ab
Marcel Cutts <marcel.cutts@red-badger.com>
2016-05-20 14:55:54 +0100

    Removed a lot of fluff

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'react-hot-loader/patch',
+    'babel-polyfill',
     'webpack-hot-middleware/client',
     './app/index',
   ],
