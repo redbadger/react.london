a0cdc147e178a28aaf14d842a4b88dd6551aefae
Marcel Cutts <marcel.cutts@red-badger.com>
2016-05-20 14:58:35 +0100

    integrated sagas

--- app/store/index.js
+++ app/store/index.js
@@ -10,9 +10,6 @@ import { fetchContentRequested, putContentRequested } from '../sagas';
 import rootReducer from '../reducers';
 
 const sagaMiddleware = createSagaMiddleware();
-sagaMiddleware.run(fetchContentRequested);
-sagaMiddleware.run(putContentRequested);
-
 const enhancer = compose(
 
   // Middleware you want to use in development:
@@ -22,8 +19,15 @@ const enhancer = compose(
   DevTools.instrument()
 );
 
+// This MUST run after mounting the middleware
+// TODO: Unhack.
+
+
 export function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
+
+  sagaMiddleware.run(fetchContentRequested);
+  sagaMiddleware.run(putContentRequested);
 
   if (module.hot) {
     module.hot.accept('../reducers', () =>
