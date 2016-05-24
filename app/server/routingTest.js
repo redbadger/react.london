2daeec56f806938bbb6e3171b8f899fe6f3743f9
Matt Phillips <mattphillips1016@gmail.com>
2016-05-24 14:44:43 +0100

    finished tests for accessing root

--- app/server/routingTest.js
+++ app/server/routingTest.js
@@ -1,96 +1,38 @@
-// import express from 'express';
-// import request from 'supertest';
-// import superagent from 'superagent';
-//
+import express from 'express';
+import request from 'supertest';
+import passport from 'passport';
+
 import { routingSetup } from './routingSetup';
-// import { build as passportMock } from './passportMock';
-//
-// describe('GET /protected-resource authorized', function () {
-//   let app = {};
-//   let agent = superagent;
-//
-//   beforeEach(function (done) {
-//     let blah = passportMock(express(), { passAuthentication: true,	userId: 1 });
-//
-//     app = routingSetup(blah.app, blah.passport);
-//
-//     // app.get('/mock/login', () => {
-//     //   return { isAuthenticated: true };
-//     // });
-//
-//     request(app)
-//       .get('/mock/login')
-//       .end(function (err, result) {
-//         if (!err) {
-//           console.log(JSON.stringify(result.headers));
-//           done();
-//         } else {
-//           done(err);
-//         }
-//       });
-//   });
-//
-//   it('should allow access to /', function (done) {
-//     var req = request(app).get('/');
-//     req.set('isAuthenticated', true);
-//
-// 		req.expect(200, done);
-//   });
-// });
 
+describe('GET /', () => {
+  it('should return 302 when redirecting user to login', done => {
+    let app = express();
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app = routingSetup(app);
 
-var express = require('express');
-var passport = require('passport');
-var request = require('supertest');
+    request(app)
+      .get('/')
+      .expect(302)
+      .expect('Location', '/login')
+      .end(done);
+  });
 
-describe('app', function() {
- describe('GET /', function() {
-   it('should return 403 when no user is logged in', function(done) {
+  it('should return 200 when user is logged in', done => {
+    let app = express();
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use((req, res, next) => {
+      req.isAuthenticated = () => true;
+      req.user = {};
+      next();
+    });
+    app = routingSetup(app);
 
-     let app = express();
-
-     app.use(passport.initialize());
-     app.use(passport.session());
-
-     app.use(function(req, res, next) {
-       req.isAuthenticated = function() {
-         return true;
-       };
-       req.user = {};
-       next();
-     });
-
-     app = routingSetup(app);
-
-     request(app)
-       .get('/')
-       .expect(200)
-       .end(done);
-   });
-   it('should return 200 when user is logged in', function(done) {
-
-     var app = express();
-     app.use(passport.initialize());
-     app.use(passport.session());
-     app.use(function(req, res, next) {
-       req.isAuthenticated = function() {
-         return true;
-       };
-       req.user = {};
-       next();
-     });
-     app.get('/', function(req, res){
-       if (!req.user || !req.isAuthenticated()){
-         return res.send(403);
-       }
-       res.send(200);
-     });
-
-     request(app)
-       .get('/')
-       .expect(200)
-       .end(done);
-
-   });
- });
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Location', '/')
+      .end(done);
+  });
 });
