f640399540ed7a985d50cd591a21db8e352e428a
Chris Goodchild <seegoodchild@gmail.com>
2016-05-26 15:22:31 +0100

    removed api in favour of persistent store, removed unused deps and removed local and remote db refs from store exports

--- app/store/index.js
+++ app/store/index.js
@@ -4,21 +4,19 @@ import React from 'react';
 
 import { createStore, compose, applyMiddleware } from 'redux';
 import { persistentStore } from 'redux-pouchdb-plus';
-import { syncDatabase } from '../api'
 
 import rootReducer from '../reducers';
 import DevTools from '../containers/DevTools';
 
 import PouchDB from 'pouchdb';
-export const localDb = new PouchDB('reactlondon');
-export const remoteDb = new PouchDB('http://localhost:5984/reactlondon')
-
-// Note: attaching PouchDB to the window is required for the chrome extension for debugging
-window.PouchDB = PouchDB
+const databaseName = 'reactlondon';
+const localDatabase = new PouchDB(databaseName);
+const remoteDatabase = new PouchDB('http://localhost:5984/' + databaseName);
 
 const enhancer = compose(
-  // Handles syncing PouchDB (local) and Redux
-  persistentStore({db: localDb}),
+  // Syncs PouchDB (local) and Redux
+  persistentStore({ db: localDatabase }),
+  
   // Required! Enable Redux DevTools with the monitors you chose
   DevTools.instrument()
 );
@@ -26,14 +24,14 @@ const enhancer = compose(
 export function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
 
-  // Handles syncing PounchDB (local) and CouchDB (remote)
-  syncDatabase(remoteDb);
+  // Syncs PounchDB (local) and CouchDB (remote)
+  localDatabase.sync(remoteDatabase, { live: true });
 
   if (module.hot) {
     module.hot.accept('../reducers', () =>
-     store.replaceReducer(require('../reducers')/*.default if you use Babel 6+ */)
+      store.replaceReducer(require('../reducers')/* .default if you use Babel 6+ */)
     );
   }
 
   return store;
-}
+};
