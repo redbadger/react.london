c7c3a35a0f6e2d0fc54fe1083050cf29d80fd575
Marcel Cutts <marcel.cutts@red-badger.com>
2016-06-02 18:19:44 +0100

    Added tests for imagefield

---
+++ app/components/Editor/ImageField/ImageField-test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import TestUtils from 'react-testutils-additions';
+
+import ImageField from './ImageField';
+
+describe('ImageField', () => {
+  it('renders the input element', () => {
+    const mockField = { touched: false, error: false };
+    const imageField = TestUtils.renderIntoDocument(<ImageField field={mockField} />);
+    const input = TestUtils.find(imageField, 'input');
+    expect(input.length).to.equal(1);
+  });
+
+  it('renders the correct speaker as a label', () => {
+    const mockField = { touched: false, error: false };
+    const mockLabel = 'Marcel';
+    const imageField = TestUtils.renderIntoDocument(
+      <ImageField
+        field={mockField}
+        label={mockLabel} />
+    );
+    const label = TestUtils.findRenderedDOMComponentWithClass(imageField, 'speaker');
+
+    expect(label.textContent).to.equal('Marcel');
+  });
+});
