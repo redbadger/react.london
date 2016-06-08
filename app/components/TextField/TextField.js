2af16bcb9cca0190f2de7c90651a7ebed14d3bff
Louis Pilfold <louis@lpil.uk>
2016-06-08 17:31:53 +0100

    TextField: PropTypes

--- app/components/TextField/TextField.js
+++ app/components/TextField/TextField.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 
 const TextField = ({ field, label }) => (
   <div>
@@ -9,5 +9,13 @@ const TextField = ({ field, label }) => (
       <span className="error">{field.error}</span>}
   </div>
 );
+
+TextField.PropTypes = {
+  label: PropTypes.string.isRequired,
+  field: PropTypes.shape({
+    touched: PropTypes.bool.isRequired,
+    error: PropTypes.string,
+  }).isRequired,
+};
 
 export default TextField;
