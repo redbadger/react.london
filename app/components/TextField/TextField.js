957ccc60f54c20679f6d5e224d2a3055ebba7ccd
Louis Pilfold <louis@lpil.uk>
2016-06-08 17:30:12 +0100

    TextField: Functional component

--- app/components/TextField/TextField.js
+++ app/components/TextField/TextField.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 
-class TextField extends Component {
-  render() {
-    const { field, label } = this.props;
-    return (
-      <div>
-        <label>{label}</label>
-        <input type="text" {...field} />
-        {field.touched &&
-          field.error &&
-          <span className="error">{field.error}</span>}
-      </div>
-    );
-  }
-}
+const TextField = ({ field, label }) => (
+  <div>
+    <label>{label}</label>
+    <input type="text" {...field} />
+    {field.touched &&
+      field.error &&
+      <span className="error">{field.error}</span>}
+  </div>
+);
 
 export default TextField;
