94e877e9bd15abe1ae945a048900b85aed87487b
Louis Pilfold <louis@lpil.uk>
2016-06-08 15:36:53 +0100

    Convert SpeakerEditor to function component

--- app/components/SpeakerEditor/SpeakerEditor.js
+++ app/components/SpeakerEditor/SpeakerEditor.js
@@ -1,38 +1,36 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
-class SpeakerEditor extends Component {
-
-  render() {
-    const { speakers, speaker, index, textField, imageField } = this.props;
-
-    return (
-      <div>
-        <h5>Talk {index + 1}</h5>
-        <Field
-          name={`${speaker}.name`}
-          label = "Name"
-          component={textField}
-        />
-        <Field
-          name={`${speaker}.title`}
-          label = "Title"
-          component={textField}
-        />
-        <Field
-          name={`${speaker}.blurb`}
-          label = "Blurb"
-          component={textField}
-        />
-        <Field
-          name={`${speaker}.picture`}
-          label = "Picture URL"
-          component={imageField}
-        />
-        <button type="button" onClick={() => speakers.remove(index)}>Remove Speaker</button>
-      </div>
-    );
-  }
-}
+const SpeakerEditor = ({
+  speakers, speaker, index, textField, imageField
+}) => (
+  <div>
+    <h5>Talk {index + 1}</h5>
+    <Field
+      name={`${speaker}.name`}
+      label = "Name"
+      component={textField}
+    />
+    <Field
+      name={`${speaker}.title`}
+      label = "Title"
+      component={textField}
+    />
+    <Field
+      name={`${speaker}.blurb`}
+      label = "Blurb"
+      component={textField}
+    />
+    <Field
+      name={`${speaker}.picture`}
+      label = "Picture URL"
+      component={imageField}
+    />
+    <button
+      type="button"
+      onClick={() => speakers.remove(index)}
+    >Remove Speaker</button>
+  </div>
+);
 
 export default SpeakerEditor;
