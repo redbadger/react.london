2c28916e2e8a32bb298d1eaf94ca2a1ab5d5dc6c
Louis Pilfold <louis@lpil.uk>
2016-06-08 16:10:29 +0100

    About: Tests

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = ({ text }) => (
     <section className="About">
-      <h1>{text.about ? text.about.title : null}</h1>
-      <div dangerouslySetInnerHTML={ text.about ? { __html: text.about.summary } : null } />
+      <h1>{text.about && text.about.title ? text.about.title : null}</h1>
+      <div dangerouslySetInnerHTML={ text.about && text.about.summary ? { __html: text.about.summary } : null } />
     </section>
   );
 
