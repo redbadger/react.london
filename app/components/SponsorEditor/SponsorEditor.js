e5a82f3a5e41ec35429dd36b7320eefa9c29480c
Louis Pilfold <louis@lpil.uk>
2016-06-09 15:23:48 +0100

    SponsorEditor: Function component

--- app/components/SponsorEditor/SponsorEditor.js
+++ app/components/SponsorEditor/SponsorEditor.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 
-class SponsorEditor extends Component {
-  render() {
-    const { sponsors, sponsor, index, textField } = this.props;
-    return (
-      <div>
-        <h5>Sponsor {index + 1}</h5>
-        <Field
-          name={`${sponsor}.name`}
-          label = "Name"
-          component={textField}
-        />
-        <Field
-          name={`${sponsor}.picture`}
-          label = "Picture URL"
-          component={textField}
-        />
-        <Field
-          name={`${sponsor}.url`}
-          label = "URL to Sponsor"
-          component={textField}
-        />
-        <button
-          className="remove-sponsor"
-          type="button"
-          onClick={ () => sponsors.remove(index)}
-          >
-            Remove Sponsor
-        </button>
-      </div>
-    );
-  }
-}
+const SponsorEditor = ({ sponsors, sponsor, index, textField }) => (
+  <div>
+    <h5>Sponsor {index + 1}</h5>
+    <Field
+      name={`${sponsor}.name`}
+      label = "Name"
+      component={textField}
+    />
+    <Field
+      name={`${sponsor}.picture`}
+      label = "Picture URL"
+      component={textField}
+    />
+    <Field
+      name={`${sponsor}.url`}
+      label = "URL to Sponsor"
+      component={textField}
+    />
+    <button
+      className="remove-sponsor"
+      type="button"
+      onClick={ () => sponsors.remove(index)}
+      >
+        Remove Sponsor
+    </button>
+  </div>
+);
 
 export default SponsorEditor;
