4448ced6cc72bd0239ffa9c17eb76a820f90eb75
Louis Pilfold <louis@lpil.uk>
2016-06-09 15:00:40 +0100

    SpeakerPreview: Tests

--- app/components/SpeakerPreview/SpeakerPreview.js
+++ app/components/SpeakerPreview/SpeakerPreview.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SpeakerPreview = ({ title, name, blurb, picture }) => (
     <section>
-      <img src={picture} />
-      <h4>{title}</h4>
-      <h5>{name}</h5>
-      <div dangerouslySetInnerHTML={ blurb ? { __html: blurb } : null } />
+      <img src={ picture } />
+      <h4>{ title }</h4>
+      <h5>{ name }</h5>
+      <div className="blurb" dangerouslySetInnerHTML={ { __html: blurb } } />
     </section>
   );
 
