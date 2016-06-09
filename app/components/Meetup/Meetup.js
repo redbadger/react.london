f2f5c68d090530e90a248536adece887dc4a9ca0
Louis Pilfold <louis@lpil.uk>
2016-06-09 11:00:11 +0100

    Meetup: Tests

--- app/components/Meetup/Meetup.js
+++ app/components/Meetup/Meetup.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 const Meetup = ({ text }) => {
   const meetup = text.meetup;
   return <section className="upcoming-meetup">
-    <h2>{meetup ? meetup.name: null}</h2>
-    <div dangerouslySetInnerHTML={ meetup ? { __html: meetup.details } : null } />
+    <h2>{meetup && meetup.name ? meetup.name: null}</h2>
+    <div
+      className="details"
+      dangerouslySetInnerHTML={ meetup ? { __html: meetup.details } : null }
+    />
     <p>{meetup ? meetup.when: null}</p>
     {(meetup && meetup.where) && <p><a target="_blank" href={meetup.where.url}>{meetup.where.text}</a></p>}
     {(meetup && meetup.signup) && <p><a target="_blank" href={meetup.signup.url}>{meetup.signup.text}</a></p>}
---
+++ app/components/Meetup/test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Meetup from './Meetup';
+
+function setup(meetupProps = {}) {
+  const props = {
+    text: {
+      meetup: meetupProps,
+    },
+  };
+  const output = shallow(<Meetup {...props} />);
+  return { props, output }
+}
+
+describe('Meetup', () => {
+  it('displays unescaped details when if present', () => {
+    const { output } = setup({ details: 'details<b>!!!</b>' });
+    const element = output.find('.details').nodes[0];
+    expect(element.props.dangerouslySetInnerHTML.__html).to.equal('details<b>!!!</b>');
+  });
+
+  it('displays name if given', () => {
+    const { output } = setup({ name: 'A name' });
+    expect(output.text()).to.include('A name');
+  });
+
+  it('displays where link if present', () => {
+    const { output } = setup({ where: { url: 'a-where-url', text: 'magic text' } });
+    const element = output.find('a[href="a-where-url"]').nodes[0];
+    expect(element).not.to.equal(undefined);
+    expect(element.props.children).to.equal('magic text');
+  });
+
+  it('displays signup url if present', () => {
+    const { output } = setup({ signup: { url: 'a-signup-url', text: 'wonderous text' } });
+    const element = output.find('a[href="a-signup-url"]').nodes[0];
+    expect(element).not.to.equal(undefined);
+    expect(element.props.children).to.equal('wonderous text');
+  });
+
+  it('displays streaming url if present', () => {
+    const { output } = setup({ streaming: { url: 'a-streaming-url', text: 'voodoo text' } });
+    const element = output.find('a[href="a-streaming-url"]').nodes[0];
+    expect(element).not.to.equal(undefined);
+    expect(element.props.children).to.equal('voodoo text');
+  });
+});
