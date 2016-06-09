4dfd200907b589470d10d41d1c5d656d1f5981c8
Louis Pilfold <louis@lpil.uk>
2016-06-09 10:27:24 +0100

    Meetup: Convert to functional component

--- app/components/Meetup/Meetup.js
+++ app/components/Meetup/Meetup.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 
-class Meetup extends Component {
-  render() {
-    const meetup = this.props.text.meetup;
-    return (
-      <section className="UpcomingMeetup">
-        <h2>{meetup ? meetup.name: null}</h2>
-        <div dangerouslySetInnerHTML={ meetup ? { __html: meetup.details } : null } />
-        <p>{meetup ? meetup.when: null}</p>
-        {(meetup && meetup.where) && <p><a target="_blank" href={meetup.where.url}>{meetup.where.text}</a></p>}
-        {(meetup && meetup.signup) && <p><a target="_blank" href={meetup.signup.url}>{meetup.signup.text}</a></p>}
-        {(meetup && meetup.streaming) && <p><a target="_blank" href={meetup.streaming.url}>{meetup.streaming.text}</a></p>}
-      </section>
-    );
-  };
-}
+const Meetup = ({ text }) => {
+  const meetup = text.meetup;
+  return <section className="upcoming-meetup">
+    <h2>{meetup ? meetup.name: null}</h2>
+    <div dangerouslySetInnerHTML={ meetup ? { __html: meetup.details } : null } />
+    <p>{meetup ? meetup.when: null}</p>
+    {(meetup && meetup.where) && <p><a target="_blank" href={meetup.where.url}>{meetup.where.text}</a></p>}
+    {(meetup && meetup.signup) && <p><a target="_blank" href={meetup.signup.url}>{meetup.signup.text}</a></p>}
+    {(meetup && meetup.streaming) && <p><a target="_blank" href={meetup.streaming.url}>{meetup.streaming.text}</a></p>}
+  </section>;
+};
 
 export default Meetup;
