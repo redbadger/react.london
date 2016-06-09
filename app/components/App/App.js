04f8d1aaba4ad62893f8a7428495d12c945d5197
Louis Pilfold <louis@lpil.uk>
2016-06-09 20:54:43 +0100

    App: Simplify

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -5,19 +5,12 @@ import Preview from '../../containers/Preview';
 
 import Radium, { Style } from 'radium';
 
-class App extends Component {
-  render() {
-    const { children } = this.props;
-    return (
-      <div style={styles}>
-        <div style={styles}>
-          <Editor />
-          <Preview />
-        </div>
-      </div>
-    );
-  }
-};
+const App = () => (
+  <div style={styles}>
+    <Editor />
+    <Preview />
+  </div>
+);
 
 const styles = {
   width: '100%',
