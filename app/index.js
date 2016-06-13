10bb44750f35aee4ae3b1748d05ad857e1409ff7
Zoe Bryant <zoeabryant@gmail.com>
2016-06-13 14:28:15 +0100

    Linting rules

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import { configureStore } from './store/';
 import App from './components/App/App';
 import Preview from './components/Preview/Preview';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <Provider store={store}>
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { persistentReducer } from 'redux-pouchdb-plus';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { routerReducer } from 'react-router-redux';
 
 const reducers = combineReducers({
   form: formReducer,
-  routing: routerReducer
+  routing: routerReducer,
 });
 
 // TODO: consider if we want routing data persisted...
