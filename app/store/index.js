dea78053baf798791e0561b98a9a5838f58d6e33
Louis Pilfold <louis@lpil.uk>
2016-06-16 11:53:31 +0100

    Remove pouch

--- app/store/index.js
+++ app/store/index.js
@@ -2,19 +2,12 @@
 import 'babel-polyfill';
 
 import { createStore, compose } from 'redux';
-import { persistentStore } from 'redux-pouchdb-plus';
-import PouchDB from 'pouchdb';
 
 import rootReducer from '../reducers';
 
-const databaseName = 'reactlondon';
-const localDatabase = new PouchDB(databaseName);
 
 const enhancer = compose(
-  // Syncs PouchDB (local) and Redux
-  persistentStore({ db: localDatabase }),
-
-  // Required! Enable Redux DevTools with the monitors you chose
+  // Enable Redux DevTools with the monitors you chose
   (
     typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
   ) ? window.devToolsExtension() : f => f
@@ -25,7 +18,7 @@ export const configureStore = initialState => {
 
   if (module.hot) {
     module.hot.accept('../reducers', () =>
-      store.replaceReducer(require('../reducers')) // .default if you use Babel 6+
+      store.replaceReducer(require('../reducers'))
     );
   }
 
