8bf33ef3e4f3f8c434e3d584beddef3af74c8792
Louis Pilfold <louis@lpil.uk>
2016-06-17 11:34:12 +0100

    Remove redundant components

--- app/containers/Editor.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import EditorComponent from '../components/Editor/Editor.js';
-
-const mapStateToProps = (state) => ({
-  content: state,
-});
-
-export default connect(mapStateToProps)(EditorComponent);
