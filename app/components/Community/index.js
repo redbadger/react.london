2743d2b49c71fb9a8fd6e94a64df95f5b9312c3e
Zoe Bryant <zoeabryant@gmail.com>
2016-06-30 10:28:57 +0100

    Failing tests, whoops

--- app/components/Community/index.js
+++ app/components/Community/index.js
@@ -7,11 +7,13 @@ import Hero from '../Hero';
 import RedBadgerBanner from '../RedBadgerBanner';
 import NavigationBar from '../NavigationBar';
 import CommunityAbout from '../CommunityAbout';
+import NextEvent from '../NextEvent';
 
 const Community = ({
   communitySummary,
   mailingListTitle,
   mailingListSummary,
+  eventTitle,
   eventDate,
   eventAddress,
   eventStartTime,
@@ -28,40 +30,13 @@ const Community = ({
         <RedBadgerBanner />
         <NavigationBar />
         <CommunityAbout communitySummary={communitySummary} />
-
-        {/* NEXT EVENT */}
-        <section className="NextEvent block">
-          <div className="content">
-            <h2 className="NextEvent__header">Next Event</h2>
-            <article className="NextEvent__section-container">
-              <div className="NextEvent__section NextEvent__section__details">
-                <h3 className="NextEvent__details__heading">July React Meetup</h3>
-                <ul className="NextEvent__details">
-                  <li>
-                    <a className="NextEvent__link--date">{eventDate}</a>
-                  </li>
-                  <li>
-                    <a className="NextEvent__link--place">{eventAddress}</a>
-                  </li>
-                  <li>
-                    <a className="NextEvent__link--time">{eventStartTime} - {eventEndTime}</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="NextEvent__section NextEvent__section__booking">
-                <h3 className="NextEvent__booking__heading">TICKETS GO LIVE</h3>
-                <p className="NextEvent__booking__text">Monday, 11 July 2016 at 13:00</p>
-                <div className="NextEvent__booking-btn__container">
-                  <a className="NextEvent__booking-btn NextEvent__booking-btn--disabled">free ticket</a>
-                </div>
-                <p className="NextEvent__live-stream-text">
-                  To get reminders about tickets and future events <a className="NextEvent__live-stream-text--link" href="#stay-tuned">subscribe here</a>
-                </p>
-
-              </div>
-            </article>
-          </div>
-        </section>
+        <NextEvent
+          eventTitle={eventTitle}
+          eventDate={eventDate}
+          eventAddress={eventAddress}
+          eventStartTime={eventStartTime}
+          eventEndTime={eventEndTime}
+        />
 
         {/* SPEAKERS */}
         <section id="speakers" className="block">
@@ -159,6 +134,7 @@ Community.propTypes = {
   communitySummary: PropTypes.string,
   mailingListTitle: PropTypes.string,
   mailingListSummary: PropTypes.string,
+  eventTitle: PropTypes.string,
   eventAddress: PropTypes.string,
   eventDate: PropTypes.string,
   eventStartTime: PropTypes.string,
--- app/components/CommunityAbout/test.js
+++ app/components/CommunityAbout/test.js
@@ -13,14 +13,14 @@ function setup({ communitySummary }) {
 describe('CommunityAbout component', () => {
   it('does not render the HTML communitySummary if there is none', () => {
     const { output } = setup({ communitySummary: undefined });
-    const element = output.find('.About__text').nodes[0];
+    const element = output.find('.CommunityAbout__text').nodes[0];
     expect(element.props.children).to.equal(undefined);
     expect(element.props.dangerouslySetInnerHTML.__html).to.equal(undefined);
   });
 
   it('renders the HTML communitySummary if there is one', () => {
     const { output } = setup({ communitySummary: 'Lorem ipsum yo' });
-    const element = output.find('.About__text').nodes[0];
+    const element = output.find('.CommunityAbout__text').nodes[0];
     expect(element.props.children).to.equal(undefined);
     const html = element.props.dangerouslySetInnerHTML.__html;
     expect(html).to.equal('Lorem ipsum yo');
