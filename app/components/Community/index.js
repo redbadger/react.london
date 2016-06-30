3a2ba8c5a86ca2dba3f75a26cecc8fdd1515c329
Zoe Bryant <zoeabryant@gmail.com>
2016-06-30 11:05:47 +0100

    Refactoring: MailingList

--- app/components/Community/index.js
+++ app/components/Community/index.js
@@ -8,6 +8,7 @@ import RedBadgerBanner from '../RedBadgerBanner';
 import NavigationBar from '../NavigationBar';
 import CommunityAbout from '../CommunityAbout';
 import NextEvent from '../NextEvent';
+import MailingList from '../MailingList';
 
 const Community = ({
   communitySummary,
@@ -52,23 +53,10 @@ const Community = ({
         </section>
 
         {/* REGISTER */}
-
-        <section id="stay-tuned" className="StayTuned block">
-          <div className="content">
-            <h3 className="StayTuned__heading">{mailingListTitle}</h3>
-            <p className="StayTuned__summary">{mailingListSummary}</p>
-
-            <div id="mc_embed_signup">
-              <form className="StayTuned__form" action="//london.us13.list-manage.com/subscribe/post?u=f3de268a0820d472cbd31f761&amp;id=c723cfd260&LOCATION=community" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" noValidate>
-                <div className="StayTuned__form__container">
-                  <label className="StayTuned__form__label" htmlFor="mce-EMAIL">Email</label>
-                  <input className="StayTuned__form__email" type="email" value="" autoComplete="off" name="EMAIL" id="mce-EMAIL" placeholder="name@address.com" required />
-                  <input className="StayTuned__form__submit" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
-                </div>
-              </form>
-            </div>
-          </div>
-        </section>
+        <MailingList
+          mailingListTitle={mailingListTitle}
+          mailingListSummary={mailingListSummary}
+        />
 
 
         <section id="event-details" className="block">
