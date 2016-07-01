351a816cc4432bda2011c6038ea55b129747af61
Louis Pilfold <louis@lpil.uk>
2016-07-01 14:59:27 +0100

    Sync community form changes

--- app/reducers/community/index.js
+++ app/reducers/community/index.js
@@ -1,9 +1,24 @@
 import { SITE_STATE_LOADED } from '../../actions/persistence';
+import { actionTypes as formActions } from 'redux-form';
+import { COMMUNITY_FORM } from '../../names/form';
+
+function handleFormChange(state, action) {
+  if (action.meta.form === COMMUNITY_FORM) {
+    return {
+      ...state,
+      [action.meta.field]: action.payload,
+    };
+  }
+  return state;
+}
 
 export default function community(state = {}, action) {
   switch (action.type) {
     case SITE_STATE_LOADED:
       return action.community;
+
+    case formActions.CHANGE:
+      return handleFormChange(state, action);
 
     default:
       return state;
--- app/reducers/community/test.js
+++ app/reducers/community/test.js
@@ -1,6 +1,8 @@
 import deepFreeze from 'deep-freeze';
 import reducer from '.';
 import { siteStateLoaded } from '../../actions/persistence';
+import { change as formFieldChange } from 'redux-form';
+import { COMMUNITY_FORM } from '../../names/form';
 
 describe('events reducer', () => {
   it('has default state', () => {
@@ -16,6 +18,37 @@ describe('events reducer', () => {
       const action = siteStateLoaded({ community });
       const state = reducer(prev, action);
       expect(state).to.deep.equal({ foo: 1, bar: 2 });
+    });
+  });
+
+  describe('redux-form/CHANGE handling', () => {
+    it('sets a value with a new value', () => {
+      const prev = deepFreeze({ color: 'yellow' });
+      const action = formFieldChange(
+        COMMUNITY_FORM, 'title', 'New Important Title'
+      );
+      const state = reducer(prev, action);
+      expect(state).to.deep.equal({
+        color: 'yellow', title: 'New Important Title',
+      });
+    });
+
+    it('inserts a value with an existing event', () => {
+      const prev = deepFreeze({ color: 'yellow' });
+      const action = formFieldChange(
+        COMMUNITY_FORM, 'color', 'blue'
+      );
+      const state = reducer(prev, action);
+      expect(state).to.deep.equal({ color: 'blue' });
+    });
+
+    it('does nothing for other forms', () => {
+      const prev = deepFreeze({ color: 'yellow' });
+      const action = formFieldChange(
+        'language', 'color', 'blue'
+      );
+      const state = reducer(prev, action);
+      expect(state).to.deep.equal({ color: 'yellow' });
     });
   });
 });
