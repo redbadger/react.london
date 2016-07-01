c6758df785847fd6097178724134c22841a5ecf3
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-07-01 11:18:21 +0100

    Re-added unchanged elements

--- app/components/Conference/index.js
+++ app/components/Conference/index.js
@@ -2,18 +2,68 @@ import React from 'react';
 import SiteFooter from '../SiteFooter';
 import Hero from '../Hero';
 import RedBadgerBanner from '../RedBadgerBanner';
-import NavigationBar from '../NavigationBar';
 
 const signupActionURL = '//london.us13.list-manage.com/subscribe/' +
   'post?u=f3de268a0820d472cbd31f761&id=c723cfd260&LOCATION=conference';
 
 const Conference = () => (
-  <div className="conference">
+  <div>
     <div id="wrapper">
-      <main>
+      <main id="conference">
         <Hero page="Conference" />
         <RedBadgerBanner />
-        <NavigationBar />
+        <nav className="NavigationBar block">
+          <ul className="NavigationBar__tabs">
+            <li><a href="/">Meetups</a></li>
+            <li><a className="NavigationBar__tab--active" href="#">Conference</a></li>
+          </ul>
+        </nav>
+        <section id="about" className="block">
+          <div className="content">
+            <h2>Let’s explore!</h2>
+            <p>Red Badger is launching a new conference focused on React in London for
+              2017 – we’re calling it <strong>React London 2017.</strong></p>
+            <p>We’re bringing together some great speakers and events
+            – get involved! More details to follow soon.</p>
+          </div>
+        </section>
+        <section id="update" className="Update block">
+          <div className="content">
+            <h3 className="Update__heading">Sign up here for conference updates</h3>
+            <div>
+              <form
+                className="Update__form"
+                action={signupActionURL}
+                method="post"
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                target="_blank"
+                noValidate
+              >
+                <div className="Update__form__container">
+                  <label className="Update__form__label" htmlFor="mce-EMAIL">Email</label>
+                  <input
+                    className="Update__form__email"
+                    type="email"
+                    value=""
+                    autoComplete="off"
+                    name="EMAIL"
+                    id="mce-EMAIL"
+                    placeholder="name@address.com"
+                    required
+                  />
+                  <input
+                    className="Update__form__submit"
+                    type="submit"
+                    value="Subscribe"
+                    name="subscribe"
+                    id="mc-embedded-subscribe"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
         <SiteFooter />
       </main>
     </div>
