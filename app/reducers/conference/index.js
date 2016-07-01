10f43794f9b22a2dff0da8a8c36427d81a60bbf6
Louis Pilfold <louis@lpil.uk>
2016-07-01 16:14:27 +0100

    Sync conference form changes

--- app/reducers/conference/index.js
+++ app/reducers/conference/index.js
@@ -1,9 +1,25 @@
 import { SITE_STATE_LOADED } from '../../actions/persistence';
+import { actionTypes as formActions } from 'redux-form';
+import { CONFERENCE_FORM } from '../../names/form';
+
+function handleFormChange(state, action) {
+  if (action.meta.form === CONFERENCE_FORM) {
+    return {
+      ...state,
+      [action.meta.field]: action.payload,
+    };
+  }
+  return state;
+}
+
 
 export default function conference(state = {}, action) {
   switch (action.type) {
     case SITE_STATE_LOADED:
       return action.conference;
+
+    case formActions.CHANGE:
+      return handleFormChange(state, action);
 
     default:
       return state;
