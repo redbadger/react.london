78ec747249a0900f57a4dcec4747adb4bdbc0310
Louis Pilfold <louis@lpil.uk>
2016-07-01 09:22:24 +0100

    EventHome has publish action dispatcher

--- app/containers/EditorHome/index.js
+++ app/containers/EditorHome/index.js
@@ -1,9 +1,26 @@
 import { connect } from 'react-redux';
 import EditorHome from '../../components/EditorHome';
+import { publishSiteState } from '../../actions/persistence';
 
 export const mapStateToProps = (state) => {
   const eventIDs = Object.keys(state.events);
-  return { eventIDs };
+  return {
+    events: state.events,
+    community: state.community,
+    conference: state.conference,
+    eventIDs,
+  };
 };
 
-export default connect(mapStateToProps)(EditorHome);
+export const mapDispatchToProps = (dispatch) => {
+  return {
+    publish: ({ events, community, conference }) => {
+      return dispatch(publishSiteState({ events, community, conference }));
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditorHome);
