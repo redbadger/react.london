33b7192d4ea29a0fa62dd602f95702047b4b3787
Louis Pilfold <louis@lpil.uk>
2016-07-05 16:00:24 +0100

    Add current static site content to app

--- shared/components/CommunityAbout/test.js
+++ shared/components/CommunityAbout/test.js
@@ -2,26 +2,17 @@ import React from 'react';
 import CommunityAbout from '.';
 import { shallow } from 'enzyme';
 
-function setup({ summary }) {
+function setup({ communitySummary }) {
   const props = {
-    summary,
+    communitySummary,
   };
   const output = shallow(<CommunityAbout {...props} />);
   return { props, output };
 }
 
 describe('CommunityAbout component', () => {
-  it('does not render the HTML summary if there is none', () => {
-    const { output } = setup({ summary: undefined });
-    const element = output.find('.CommunityAbout__text').first();
-    expect(element.text()).to.equal('');
-  });
-
-  it('renders the HTML summary if there is one', () => {
-    const { output } = setup({ summary: 'Lorem ipsum yo' });
-    const element = output.find('.CommunityAbout__text').first();
-    expect(element.props.children).to.equal(undefined);
-    const html = element.props().dangerouslySetInnerHTML.__html;
-    expect(html).to.equal('Lorem ipsum yo');
+  it('renders the summary', () => {
+    const { output } = setup({ communitySummary: 'Hi!' });
+    expect(output.text()).to.include('Hi!');
   });
 });
