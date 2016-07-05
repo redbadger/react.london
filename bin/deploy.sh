f392a8d8f0dcb2a4ee3c9ae286cbd225d290dbe0
Louis Pilfold <louis@lpil.uk>
2016-07-05 16:59:51 +0100

    Unify deployment process for staging and production

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -22,18 +22,10 @@ set -o pipefail
 eval $(aws ecr get-login --region=$AWS_REGION)
 
 # Build Docker image
-if [ "$ENV" == "production" ]
-then
-  VERSION=$RELEASE_TAG
-  docker build -t $APP_NAME:$RELEASE_TAG .
-  docker tag $APP_NAME:$RELEASE_TAG $ECR_REPO:$RELEASE_TAG
-  docker push $ECR_REPO:$RELEASE_TAG
-else
-  VERSION=$ENV
-  docker build -t $APP_NAME:$ENV .
-  docker tag $APP_NAME:$ENV $ECR_REPO:$ENV
-  docker push $ECR_REPO:$ENV
-fi
+VERSION=$RELEASE_TAG
+docker build -t $APP_NAME:$RELEASE_TAG .
+docker tag $APP_NAME:$RELEASE_TAG $ECR_REPO:$RELEASE_TAG
+docker push $ECR_REPO:$RELEASE_TAG
 
 # Apply docker image path to Dockerrun.aws.json template
 cp Dockerrun.aws.json.template Dockerrun.aws.json
@@ -45,16 +37,12 @@ ZIP_FILE=$VERSION.zip
 zip -r $ZIP_FILE Dockerrun.aws.json
 aws s3 cp $ZIP_FILE s3://$EB_BUCKET/$APP_NAME/$ZIP_FILE
 
-# In production, create a new app version
-if [ "$ENV" == "production" ]
-then
-  # Create a new application version with the zipped up Dockerrun file
-  aws elasticbeanstalk create-application-version \
-    --application-name $APP_NAME \
-    --version-label $VERSION \
-    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$APP_NAME/$ZIP_FILE \
-    --region $AWS_REGION
-fi
+# Create a new application version with the zipped up Dockerrun file
+aws elasticbeanstalk create-application-version \
+  --application-name $APP_NAME \
+  --version-label $VERSION \
+  --source-bundle S3Bucket=$EB_BUCKET,S3Key=$APP_NAME/$ZIP_FILE \
+  --region $AWS_REGION
 
 # Update the environment to use the new application version
 aws elasticbeanstalk update-environment \
