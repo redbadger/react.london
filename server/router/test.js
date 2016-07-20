241f49a3ed1733fc1e8b8c709033bfa8b240b711
Louis Pilfold <louis@lpil.uk>
2016-07-20 15:55:58 +0100

    Reenable router happy path tests

--- server/router/test.js
+++ server/router/test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from '../app';
+import { useDummyData } from '../../test/mock-data-source';
 
 describe('GET /__health__', () => {
   it('returns 200, to be checked by load balancer', done => {
@@ -20,9 +21,9 @@ describe('GET /', () => {
   });
 });
 
-// Pending due to data layer being implemented
-xdescribe('GET /conference', () => {
+describe('GET /conference', () => {
   it('/renders OK', done => {
+    useDummyData();
     request(app)
       .get('/conference')
       .expect(200)
@@ -30,12 +31,14 @@ xdescribe('GET /conference', () => {
   });
 });
 
-// Pending due to data layer being implemented
-xdescribe('GET /community', () => {
+describe('GET /community', () => {
   it('/renders OK', done => {
+    useDummyData();
     request(app)
       .get('/community')
       .expect(200)
       .end(done);
   });
 });
+
+// TODO: Sad path tests. And also sad path implementation.
