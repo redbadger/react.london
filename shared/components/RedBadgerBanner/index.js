19bbf8e5262064c8a1f58d68bd91c54de306da79
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-08 17:30:50 +0100

    Added experimental work for badger challenge

--- shared/components/RedBadgerBanner/index.js
+++ shared/components/RedBadgerBanner/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import Popover from '@terebentina/react-popover';
+import TypeWriter from 'react-typewriter';
 
 const RedBadgerBanner = () => (
   <section className="RedBadgerBanner block">
     <div className="content space-between">
-      <object
-        className="RedBadgerBanner__logo"
-        data="/img/SVG/Badger.svg"
-        type="image/svg+xml"
-      >
-        <img
-          className="RedBadgerBanner__logo"
-          srcSet="/img/PNG/ReactLondon_SaveTheDate_Icons_x2-01.png"
-          src="/img/PNG/ReactLondon_SaveTheDate_Icons-01.png"
-          alt="Red Badger logo"
-        />
-      </object>
+      <div>
+      <Popover position="right" isOpen={true} className="awesome" trigger={<object
+              className="RedBadgerBanner__logo"
+              data="/img/SVG/Badger.svg"
+              type="image/svg+xml"
+            >
+              <img
+                className="RedBadgerBanner__logo"
+                srcSet="/img/PNG/ReactLondon_SaveTheDate_Icons_x2-01.png"
+                src="/img/PNG/ReactLondon_SaveTheDate_Icons-01.png"
+                alt="Red Badger logo"
+              />
+            </object>}>
+            <TypeWriter typing={1} minDelay={60}>Please help me! I was caught in a Badger Trap.</TypeWriter>
+      </Popover>
+      </div>
     </div>
   </section>
 );
