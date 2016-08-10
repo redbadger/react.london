55c7bed6c0df11b46816c05b2750a7b04f19ef23
Sammy Isseyegh <sammyisseyegh@gmail.com>
2016-08-10 15:52:33 +0100

    make the tests pass

--- shared/containers/Community/test.js
+++ shared/containers/Community/test.js
@@ -10,10 +10,21 @@ describe('Community mapStateToProps', () => {
   it('gets community values', () => {
     expect(mapStateToProps(state)).to.deep.equal({
       summary: 'community summary!',
+      shhhhhhhh: {
+        flip: 'Gur orfg jnl gb',
+        open: ' yrnea vf ol qbvat',
+        xoyo: ' vg jebat. - Qna Noenzbi',
+      },
     });
   });
 
   it('defaults if no values', () => {
-    expect(mapStateToProps({})).to.deep.equal({});
+    expect(mapStateToProps({})).to.deep.equal({
+      shhhhhhhh: {
+        flip: 'Gur orfg jnl gb',
+        open: ' yrnea vf ol qbvat',
+        xoyo: ' vg jebat. - Qna Noenzbi',
+      },
+    });
   });
 });
