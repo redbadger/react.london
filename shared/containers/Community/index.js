dfd75e1d0a4bdc7d11f414e2c71a5bf9b88551da
Sammy Isseyegh <sammyisseyegh@gmail.com>
2016-08-10 12:52:45 +0100

    add puzzle!

--- shared/containers/Community/index.js
+++ shared/containers/Community/index.js
@@ -2,7 +2,14 @@ import { connect } from 'react-redux';
 import Community from '../../components/Community';
 
 export const mapStateToProps = (state) => {
-  return state.community || {};
+  return {
+    ...state.community,
+    shhhhhhhh: {
+      flip: 'Gur orfg jnl gb',
+      open: ' yrnea vf ol qbvat',
+      xoyo: ' vg jebat. - Qna Noenzbi',
+    },
+  } || {};
 };
 
 export default connect(mapStateToProps)(Community);
