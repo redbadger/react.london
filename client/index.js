6ed49d649bdb6a31ac146f05052f5d8d087b0933
Dan Nuttall <pixelblend@gmail.com>
2016-08-15 11:18:08 +0100

    Switch components for conference and community sites depending on meetup request hostname

--- client/index.js
+++ client/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Router, browserHistory } from 'react-router';
-import routes from '../shared/routes/community-routes';
+import communityRoutes from '../shared/routes/community-routes';
+import conferenceRoutes from '../shared/routes/conference-routes';
 import reducer from '../shared/reducers';
+import isMeetupRequest from '../shared/utilities/meetup-request';
 
 const initialState = window.__INITIAL_STATE__;
 const store = createStore(reducer, initialState);
 
-const components = (
-  <Provider store={store}>
+let components;
+
+if (isMeetupRequest(window.location)) {
+  components = (
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        {communityRoutes}
+      </Router>
+    </Provider>
+  );
+} else {
+  components = (
     <Router history={browserHistory}>
-      {routes}
+      {conferenceRoutes}
     </Router>
-  </Provider>
-);
+  );
+}
 
 ReactDOM.render(components, document.getElementById('main'));
