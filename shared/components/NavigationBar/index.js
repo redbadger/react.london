16db557ae19a8ba2d02b26ee4a41ea246ef52db8
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-15 16:51:35 +0100

    Added buy tickets button and responsiveness

--- shared/components/NavigationBar/index.js
+++ shared/components/NavigationBar/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 const NavigationBar = () => (
   <nav className={'NavigationBar block NavigationBar--Conference'}>
-  <input className="trigger" type="checkbox" id="mainNavButton"/>
-  <label htmlFor="mainNavButton">Menu</label>
-  <ul>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Speakers</a></li>
-    <li><a href="#">Schedule</a></li>
-    <li><a href="#">Venue</a></li>
-    <li><a href="#">Partners</a></li>
-    <li><a href="#">Jobs</a></li>
-  </ul>
+    <input className="trigger" type="checkbox" id="mainNavButton" />
+    <label htmlFor="mainNavButton">Menu</label>
+    <ul>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Speakers</a></li>
+      <li><a href="#">Schedule</a></li>
+      <li><a href="#">Venue</a></li>
+      <li><a href="#">Partners</a></li>
+      <li><a href="#">Jobs</a></li>
+    </ul>
+    <div className='wild'>
+      <a href="#" className='ticket__button'>Buy tickets</a>
+    </div>
   </nav>
 );
 
