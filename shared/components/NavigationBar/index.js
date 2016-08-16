a6917d360f4be748f13ea788eb7927c13e374923
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-16 12:30:19 +0100

    Css tweaks, cleaning and commenting

--- shared/components/NavigationBar/index.js
+++ shared/components/NavigationBar/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const NavigationBar = () => (
   <nav className={'NavigationBar block NavigationBar--Conference'}>
-    <div className='links_container'>
-      <input className="trigger" type="checkbox" id="mainNavButton" />
-      <label htmlFor="mainNavButton">Menu</label>
+    <div className="NavigationBar__links-container">
+      <input className="trigger" type="checkbox" id="burger" />
+      <label htmlFor="burger">Menu</label>
       <ul>
         <li><a href="#">About</a></li>
         <li><a href="#">Speakers</a></li>
         <li><a href="#">Schedule</a></li>
-        <li><a href="#" className='active'>Venue</a></li>
+        <li><a href="#" className="active">Venue</a></li>
         <li><a href="#">Partners</a></li>
         <li><a href="#">Jobs</a></li>
       </ul>
