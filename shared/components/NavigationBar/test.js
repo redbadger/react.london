87da6beb6656a209c3f0aa9b2d9aeed4afe6af40
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-16 14:24:32 +0100

    Fixed nav bar test

--- shared/components/NavigationBar/test.js
+++ shared/components/NavigationBar/test.js
@@ -5,6 +5,6 @@ import { shallow } from 'enzyme';
 describe('NavigationBar component', () => {
   it('renders the correct number of links', () => {
     const output = shallow(<NavigationBar />);
-    expect(output.find('li').length).to.equal(2);
+    expect(output.find('li').length).to.equal(6);
   });
 });
