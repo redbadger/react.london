714074bd8245cf4ceae39cb021a969452bd00b0a
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-16 10:42:34 +0100

    Minor tweaks to match design

--- shared/components/NavigationBar/index.js
+++ shared/components/NavigationBar/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const NavigationBar = () => (
   <nav className={'NavigationBar block NavigationBar--Conference'}>
-    <input className="trigger" type="checkbox" id="mainNavButton" />
-    <label htmlFor="mainNavButton">Menu</label>
-    <ul>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Speakers</a></li>
-      <li><a href="#">Schedule</a></li>
-      <li><a href="#">Venue</a></li>
-      <li><a href="#">Partners</a></li>
-      <li><a href="#">Jobs</a></li>
-    </ul>
-    <div className='wild'>
-      <a href="#" className='ticket__button'>Buy tickets</a>
+    <div>
+      <input className="trigger" type="checkbox" id="mainNavButton" />
+      <label htmlFor="mainNavButton">Menu</label>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Speakers</a></li>
+        <li><a href="#">Schedule</a></li>
+        <li><a href="#">Venue</a></li>
+        <li><a href="#">Partners</a></li>
+        <li><a href="#">Jobs</a></li>
+      </ul>
+    </div>
+    <div className="ticket__button__container">
+      <a href="#" className="ticket__button">Tickets</a>
     </div>
   </nav>
 );
