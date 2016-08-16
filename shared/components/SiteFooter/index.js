08fb8c0addb102ae4ad54663a66db4adce5af1b3
Louis Pilfold <louis@lpil.uk>
2016-08-16 12:20:45 +0100

    Link sections for SiteFooters

--- shared/components/SiteFooter/index.js
+++ shared/components/SiteFooter/index.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const SiteFooter = () => (
-  <footer className="SiteFooter block">
-    <div className="SiteFooter__conference block">
-      <h2>
-        <span className="text-background">
-          React London 2017
-        </span>
-      </h2>
-      <p>
-        <span className="text-background">
-          We're launching a new conference<br />focused on React
-        </span>
-      </p>
-      <a className="SiteFooter__conference-cta" href="#">
-        <span className="text-background">
-          Find out more
-        </span>
-      </a>
+const pageType = PropTypes.oneOf([
+  'Conference',
+  'Community',
+]).isRequired;
+
+const linkType = PropTypes.shape({
+  text: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+});
+
+function Banner({ page }) {
+  if (page === 'Community') {
+    return (
+      <div className="SiteFooter__conference block">
+        <h2>
+          <span className="text-background">
+            React London 2017
+          </span>
+        </h2>
+        <p>
+          <span className="text-background">
+            We're launching a new conference<br />focused on React
+          </span>
+        </p>
+        <a className="SiteFooter__conference-cta" href="#">
+          <span className="text-background">
+            Find out more
+          </span>
+        </a>
+      </div>
+    );
+  }
+  return (
+    null
+  );
+}
+
+Banner.propTypes = {
+  page: pageType,
+};
+
+function LinkList({ title, className, links = [] }) {
+  if (links.length < 1) { return null; }
+  return (
+    <div className={'SiteFooter__' + className}>
+      <h2>{title}</h2>
+      <ul className="SiteFooter__links">
+        {links.map(({ url, text }, index) => (
+          <li key={index}><a href={url}>{text}</a></li>
+        ))}
+      </ul>
     </div>
+  );
+}
+
+LinkList.propTypes = {
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  links: PropTypes.arrayOf(linkType),
+};
+
+const SiteFooter = ({ page, seriousLinks, usefulLinks }) => (
+  <footer className={'block SiteFooter SiteFooter__' + page}>
+    <Banner page={page} />
 
     <div className="SiteFooter__content block">
       <div className="SiteFooter__content-container content">
@@ -41,49 +86,55 @@ const SiteFooter = () => (
         </div>
 
         <div className="SiteFooter__additional-info">
-          <h2>Talk to us</h2>
-          <div className="SiteFooter__links SiteFooter__social-links">
-            <a
-              className="SiteFooter__social-link SiteFooter__mail"
-              href="mailto:hello@react.london"
-            >
-            </a>
-            <a
-              className="SiteFooter__social-link SiteFooter__slack"
-              href="http://slack.red-badger.com/"
-              target="_blank"
-            >
-            </a>
-            <a
-              className="SiteFooter__social-link SiteFooter__youtube"
-              href="https://www.youtube.com/playlist?list=PLW6ORi0XZU0BL3Up9mXpP75ilJBDOjMsQ"
-              target="_blank"
-            >
-            </a>
-            <a
-              className="SiteFooter__social-link SiteFooter__twitter"
-              href="https://twitter.com/ReactLondon_"
-              target="_blank"
-            >
-            </a>
-            {/*
+          <div className="SiteFooter__social">
+            <h2>Talk to us</h2>
+            <div className="SiteFooter__links SiteFooter__social-links">
               <a
-                className="SiteFooter__social-link SiteFooter__instagram"
-                href="#TODO"
+                className="SiteFooter__social-link SiteFooter__mail"
+                href="mailto:hello@react.london"
+              >
+              </a>
+              <a
+                className="SiteFooter__social-link SiteFooter__slack"
+                href="http://slack.red-badger.com/"
                 target="_blank"
               >
               </a>
-            */}
+              <a
+                className="SiteFooter__social-link SiteFooter__youtube"
+                href="https://www.youtube.com/playlist?list=PLW6ORi0XZU0BL3Up9mXpP75ilJBDOjMsQ"
+                target="_blank"
+              >
+              </a>
+              <a
+                className="SiteFooter__social-link SiteFooter__twitter"
+                href="https://twitter.com/ReactLondon_"
+                target="_blank"
+              >
+              </a>
+              {/*
+                <a
+                  className="SiteFooter__social-link SiteFooter__instagram"
+                  href="#TODO"
+                  target="_blank"
+                >
+                </a>
+              */}
+            </div>
           </div>
 
-          <h2>Serious Stuff</h2>
-          <ul className="SiteFooter__links SiteFooter__serious">
-            <li><a href="#">Our Code of Conduct</a></li>
-          </ul>
+          <LinkList title="Serious Stuff" className="serious" links={seriousLinks} />
+          <LinkList title="Useful Links" className="useful" links={usefulLinks} />
         </div>
       </div>
     </div>
   </footer>
 );
+
+SiteFooter.propTypes = {
+  page: pageType,
+  seriousLinks: LinkList.propTypes.links,
+  usefulLinks: LinkList.propTypes.links,
+};
 
 export default SiteFooter;
