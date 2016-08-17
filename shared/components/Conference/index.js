a506be329206ea94787adb35d87ac9bbac183552
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-08-17 11:16:44 +0100

    Fixed footer css

--- shared/components/Conference/index.js
+++ shared/components/Conference/index.js
@@ -7,44 +7,43 @@ import ConferenceAbout from '../ConferenceAbout';
 import NextConferenceEvent from '../NextConferenceEvent';
 import ShareYourIdeas from '../ShareYourIdeas';
 
-const sssh = {
-  whatdoesthe___say: {
-    flip: 'Gur orfg jnl gb',
-    open: ' yrnea vf ol qbvat',
-    xoyo: ' vg jebat. - Qna Noenzbi',
-  },
-  bite: {
-    alias: 'Yvfc vfa\'g n ynathntr,',
-    noop: ' vg\'f n ohvyqvat zngrevny.',
-    tail: ' - Nyna Xnl',
-  },
-  other: {
-    moo: {
-      category: 'Ižql xóq , nxb xrol gra ',
-      ouch: ', xgb fxbačí hqežnť iáš xóq ohqr aáfvyaý cflpubcng ,',
-      watch: ' xgbeý ivr, xqr žvwrgr . - Evpx Bfobear',
-    },
-  },
+const first = {
+  flip: 'Gur orfg jnl gb',
+  open: ' yrnea vf ol qbvat',
+  xoyo: ' vg jebat. - Qna Noenzbi',
+};
+const second = {
+  alias: 'Yvfc vf trra gnny ,',
+  noop: ' urg vf rra obhjzngrevnny.',
+  tail: ' - Nyna Xnl',
+};
+const third = {
+  category: 'Ižql xóq , nxb xrol gra ',
+  ouch: ', xgb fxbačí hqežnť iáš xóq ohqr aáfvyaý cflpubcng ,',
+  watch: ' xgbeý ivr, xqr žvwrgr . - Evpx Bfobear',
 };
 
-const Conference = () => (
-  <div className="conference">
-    <div id="wrapper">
-      <main>
-        <Hero page="Conference" />
-        <RedBadgerBanner />
-        <ConferenceAbout />
-        <NextConferenceEvent sssh={sssh} />
-        <MailingList
-          mailingListTitle="Stay tuned"
-          mailingListSummary="Get ticket reminders and event information about the conference."
-          page="conference"
-        />
-        <ShareYourIdeas />
-        <SiteFooter />
-      </main>
+const Conference = () => {
+  console.log('Hey, there is something to find here=urer! Email us the decrypted message to win a free ticket!'); // eslint-disable-line max-len, no-console
+  return (
+    <div className="conference">
+      <div id="wrapper">
+        <main>
+          <Hero page="Conference" sssh={first} />
+          <RedBadgerBanner />
+          <ConferenceAbout />
+          <NextConferenceEvent sssh={second} />
+          <MailingList
+            mailingListTitle="Stay tuned"
+            mailingListSummary="Get ticket reminders and event information about the conference."
+            page="conference"
+          />
+          <ShareYourIdeas />
+          <SiteFooter sssh={third} />
+        </main>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Conference;
