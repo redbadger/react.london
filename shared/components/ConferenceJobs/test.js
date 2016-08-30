e8f894af3d26750d192f9440acc67e7de049f3ce
Zoe Bryant <zoeabryant@gmail.com>
2016-08-30 12:14:17 +0100

    Adding better tests for ConferenceJobs component

--- shared/components/ConferenceJobs/test.js
+++ shared/components/ConferenceJobs/test.js
@@ -3,7 +3,29 @@ import { shallow } from 'enzyme';
 import ConferenceJobs from '.';
 
 describe('ConferenceJobs component', () => {
-  it('renders successfully with no props', () => {
-    shallow(<ConferenceJobs />);
+  it('renders a no jobs message when given no props', () => {
+    const wrapper = shallow(<ConferenceJobs />);
+    expect(wrapper.find('.ConferenceJobs__no-jobs')).to.have.length(1);
+  });
+
+  it('renders a no jobs message when given partners without jobs', () => {
+    const wrapper = shallow(<ConferenceJobs
+      gold={[{jobs: []}, {jobs: []},]}
+      silver={[{jobs: []}, {jobs: []},]}
+      bronze={[{jobs: []}, {jobs: []},]}
+      />);
+    expect(wrapper.find('.ConferenceJobs__no-jobs')).to.have.length(1);
+  });
+
+  it('renders successfully when given partners with jobs', () => {
+    const partnerWithJobs = {
+      jobs: [{}, {}]
+    }
+    const wrapper = shallow(<ConferenceJobs
+      gold={[partnerWithJobs, partnerWithJobs,]}
+      silver={[partnerWithJobs, partnerWithJobs,]}
+      bronze={[partnerWithJobs, partnerWithJobs,]}
+      />);
+    expect(wrapper.find('.ConferenceJobs__no-jobs')).to.have.length(0);
   });
 });
