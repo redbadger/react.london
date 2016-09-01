4507c2e968dfa943f7832b29c6233d4b9d7941d3
Alex Savin <karismafilms@gmail.com>
2016-09-01 11:11:58 +0100

    Initial implementation of SpeakersList component

---
+++ shared/components/SpeakersList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SpeakersList = () => {
+  return (
+    <section>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default SpeakersList;
