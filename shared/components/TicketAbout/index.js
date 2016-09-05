557a3453fd2506626dbd6e3e999a040be8e28b89
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-09-05 17:40:05 +0100

    Removed duplicate Ticket About copy

--- shared/components/TicketAbout/index.js
+++ shared/components/TicketAbout/index.js
@@ -6,11 +6,7 @@ const TicketAbout = () => {
       <div className="content">
         <h2>Tickets</h2>
         <p>
-          To help more people from groups often underrepresented at tech conferences
-          attend React London 2017, we&#39;re offering a limited number of diversity tickets.
-          Watch this space for more details.
-        </p>
-        <p>We have kept the ticket prices for React London 2017 as low as possible.
+          We have kept the ticket prices for React London 2017 as low as possible.
           Any profit will be reinvested back into the React London
           community through our meetups and more diversity tickets ar React London 2018
         </p>
