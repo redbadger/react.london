ec6859e4264d1649880000ade2a8f883bc413a9c
Alex Savin <karismafilms@gmail.com>
2016-09-07 17:14:14 +0100

    Adding mailing list component to the speaker list

--- shared/components/SpeakersPage/index.js
+++ shared/components/SpeakersPage/index.js
@@ -1,12 +1,18 @@
 import React, { PropTypes } from 'react';
 import SpeakerListIntro from '../SpeakerListIntro';
 import SpeakerList, { speakerType } from '../SpeakersList';
+import MailingList from '../MailingList';
 
 const SpeakersPage = ({ speakers }) => {
   return (
     <div className="SpeakersPage">
       <SpeakerListIntro />
       <SpeakerList speakers={speakers} />
+      <MailingList
+        mailingListTitle="Stay tuned"
+        mailingListSummary="Get ticket reminders and event information about the conference."
+        page="conference"
+      />
     </div>
   );
 };
