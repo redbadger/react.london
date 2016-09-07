34a3c94bb62436b798c5fde63cd01ce05484035e
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-09-07 10:40:12 +0100

    Added updated SVG and PNG for 404 page

--- shared/components/Conference404Component/index.js
+++ shared/components/Conference404Component/index.js
@@ -7,9 +7,16 @@ const Conference404Component = () => (
       <p className="Conference404Component__text">
       </p>
       <p className="Conference404Component__text--bold">
-        <strong>
-          Oops it seems as though this page went walkies...
-        </strong>
+        <object
+          data={'/img/SVG/404_missing_react.svg'}
+          type="image/svg+xml"
+        >
+          <img
+            srcSet={'/img/PNG/404_missing_react.png'}
+            src={'/img/PNG/404_missing_react.png'}
+            alt="Red Badger logo"
+          />
+        </object>
       </p>
     </div>
   </section>
