883b2afe07bdcdf6a17072d6785d6ddbaa731d96
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-09-08 11:38:48 +0100

    Removed svgs and replaced with incline svg css

--- shared/components/ErrorPage404/index.js
+++ shared/components/ErrorPage404/index.js
@@ -5,17 +5,7 @@ const ErrorPage404 = () => (
   <section className="block">
     <div className="content">
       <div className="ErrorPage404">
-        <object
-          data={'/img/SVG/404_missing_react.svg'}
-          type="image/svg+xml"
-          className="ErrorPage404__image"
-        >
-          <img
-            srcSet={'/img/PNG/404_missing_react.png'}
-            src={'/img/PNG/404_missing_react.png'}
-            alt="Red Badger logo"
-          />
-        </object>
+        <div className="ErrorPage404__image"></div>
         <h2 className="ErrorPage404__text"><strong>Ooops</strong></h2>
         <h2 className="ErrorPage404__text">
           Seems like the page you are looking for has gone walkies!
