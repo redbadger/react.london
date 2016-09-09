9b75c1eb4c4ae87dc504b425392f2feb3ff86c35
Louis Pilfold <louis@lpil.uk>
2016-09-09 16:16:12 +0100

    Log errors on server

--- server/routers/handle-error.js
+++ server/routers/handle-error.js
@@ -1,9 +1,11 @@
 import ErrorPage500 from '../../shared/components/ErrorPage500';
 import { renderToString } from 'react-dom/server';
 import React from 'react';
+import logger from '../logger';
 
 export default function makeErrorHandler(res, Layout) {
-  return () => {
+  return (error) => {
+    logger.error(error);
     const content = renderToString(
       <Layout>
         <ErrorPage500 />
