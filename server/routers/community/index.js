0e857929f9dd340863d83fe1efa9f9db8bf92ac0
Andrew <andrew.bestbier@red-badger.com>
2016-09-12 10:19:15 +0100

    Revert "Log server errors."

--- server/routers/community/index.js
+++ server/routers/community/index.js
@@ -1,22 +1,30 @@
 import { CONFERENCE_URL } from '../../constants';
+import { renderToString } from 'react-dom/server';
+import React from 'react';
 import { getCommunityState } from '../../data';
 import communityRoutes from '../../../shared/routes/community-routes';
 import communityData from '../../community-data';
 import useRouter from '../../use-router';
+import ErrorPage500 from '../../../shared/components/ErrorPage500';
 import CommunityLayout from '../../../shared/components/CommunityLayout';
-import makeErrorHandler from '../handle-error';
 
 function router(req, res) {
   if (req.path === '/conference') {
     return res.redirect(CONFERENCE_URL);
   }
-  getCommunityState()
-    .then(state => {
-      const initialState = communityData(state);
-      const routes = communityRoutes(initialState);
-      useRouter({ res, req, routes, initialState });
-    })
-    .catch(makeErrorHandler(res, CommunityLayout));
+  getCommunityState().then(state => {
+    const initialState = communityData(state);
+    const routes = communityRoutes(initialState);
+    useRouter({ res, req, routes, initialState });
+  })
+    .catch(() => {
+      const content = renderToString(
+        <CommunityLayout>
+          <ErrorPage500 />
+        </CommunityLayout>
+      );
+      res.render('index', { content });
+    });
 }
 
 export default router;
