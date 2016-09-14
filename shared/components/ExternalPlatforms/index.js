266b6d207138098ac523bce0a2fea01e2a0b665f
Andrew Bestbier <andrew.bestbier@gmail.com>
2016-09-14 14:32:47 +0100

    Fixed linting errors on externalplatforms component

--- shared/components/ExternalPlatforms/index.js
+++ shared/components/ExternalPlatforms/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 
 const ExternalPlatforms = () => (
   <div className="ExternalPlatforms">
-      <a
-        target="_blank"
-        rel="noopener"
-        href={SLACK_URL}
-        className="ExternalPlatforms__btn ExternalPlatforms__btn--slack"
-      >Join us on Slack</a>
-      <a
-        target="_blank"
-        rel="noopener"
-        href={SLACK_URL}
-        className="ExternalPlatforms__btn ExternalPlatforms__btn--youtube"
-      >Join us on Slack</a>
+    <a
+      target="_blank"
+      rel="noopener"
+      href={SLACK_URL}
+      className="ExternalPlatforms__btn ExternalPlatforms__btn--slack"
+    >Join us on Slack</a>
+    <a
+      target="_blank"
+      rel="noopener"
+      href={SLACK_URL}
+      className="ExternalPlatforms__btn ExternalPlatforms__btn--youtube"
+    >Join us on Slack</a>
   </div>
 );
 
