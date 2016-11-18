43dc8822751c1bf71ebd9a5c327c5b9f2daf7126
smeb <bryves@gmail.com>
2016-11-18 08:11:09 +0000

    Refactor SpeakersAbout to group by media

--- shared/components/SpeakersPage/index.js
+++ shared/components/SpeakersPage/index.js
@@ -12,6 +12,13 @@ const SpeakersPage = (props) => {
     <div className="SpeakersPage">
       <SpeakersPageAbout />
       <SpeakersPageContent {...props} />
+      <div className="SpeakersPage__no-cfp">
+        <p>
+          In order to ensure the day is packed with the freshest,
+          newest content our call for papers will not open until November.
+          We will announce the full speaker line up in February.
+        </p>
+      </div>
       <MailingList
         mailingListTitle="Stay tuned"
         mailingListSummary="Get ticket reminders and event information about the conference."
--- shared/components/SpeakersPageContent/index.js
+++ shared/components/SpeakersPageContent/index.js
@@ -5,20 +5,13 @@ import speakerType from '../../prop-types/speaker-type';
 const SpeakersPageContent = ({ speakers }) => (
   <section className="SpeakersPageContent block">
     <div className="content">
-      <div className="speakers-panel-description">
+      <div className="SpeakersPageContent__preamble">
         <p>
           We are thrilled to have members of the Facebook team joining
           us on the day for a panel discussion.
         </p>
       </div>
       <SpeakerList speakers={speakers} />
-      <div className="no-cfp-announcement">
-        <p>
-          In order to ensure the day is packed with the freshest,
-          newest content our call for papers will not open until November.
-          We will announce the full speaker line up in February.
-        </p>
-      </div>
     </div>
   </section>
 );
