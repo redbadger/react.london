2cc86dad4ffa853d9a009cd9633fe72d955da0b6
JR Granell <john-robert.granell@red-badger.com>
2017-01-19 17:29:23 +0000

    Styling updates

--- shared/components/SpeakersPageContent/index.js
+++ shared/components/SpeakersPageContent/index.js
@@ -1,9 +1,9 @@
 import React, { PropTypes } from 'react';
 import SpeakerPageTalkList from '../SpeakerPageTalkList';
 import speakerType from '../../prop-types/speaker-type';
+import { ExternalLink } from '../ExternalLink';
 
 const SpeakersPageContent = (props) => {
-  console.log(props);
   return (
     <section className="SpeakersPageContent block">
       <div className="content">
@@ -13,6 +13,24 @@ const SpeakersPageContent = (props) => {
           us on the day for a panel discussion.
         </p>
         <SpeakerPageTalkList talks={props.talks} />
+        <div className="SpeakersPageContent__footer">
+          <div className="SpeakersPageContent__footer__title">
+            "Simply put everyone is welcome!"
+          </div>
+          <div className="SpeakersPageContent__footer__text">
+            Read <ExternalLink href="">
+              10 ways we’ve made React London 2017 an inclusive event
+            </ExternalLink>
+          </div>
+
+          <ExternalLink
+            href="https://ti.to/red-badger/react-london-2017/"
+            className="SpeakersPageContent__footer__button"
+          >
+              Get me a ticket
+          </ExternalLink>
+
+        </div>
       </div>
     </section>
   );
