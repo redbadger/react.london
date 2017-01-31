c9ee176d76f591ff8e06a10efca273915a27350e
Alexander Savin <asavin@users.noreply.github.com>
2017-01-31 16:34:51 +0000

    Revert "Deactivated 'Call for papers' and 'Become a speaker' links"

--- shared/components/SpeakerPlaceholder/index.js
+++ shared/components/SpeakerPlaceholder/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ExternalLink } from '../ExternalLink';
 const Speaker = () => {
   const avatar = '/img/PNG/SpeakerWhite.png';
   return (
@@ -7,8 +8,14 @@ const Speaker = () => {
         <img className="Speaker__photo--img" src={avatar} alt={'Speaker placeholder'} />
       </figure>
       <h5 className="Speaker__name">
-        <div className="Speaker__name--bold__placeholder">More speakers to be announced soon.</div>
+        <div className="Speaker__name--bold__placeholder">Could this be you?</div>
       </h5>
+      <ExternalLink
+        href="https://www.papercall.io/reactlondon2017"
+        className="Speaker__button"
+      >
+        Become a speaker
+      </ExternalLink>
     </article>
   );
 };
