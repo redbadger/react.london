5a579de25a5dd9f7e7c2cc414a7fc3079309831f
Alex Savin <karismafilms@gmail.com>
2017-02-10 11:26:55 +0000

    Fix for #300 design fixes for Opportunity tickets

--- shared/components/OpportunityTickets/index.js
+++ shared/components/OpportunityTickets/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const formUrl = 'https://redbadger.typeform.com/to/eacPbb';
-
 const OpportunityTickets = () => {
   return (
     <section className="block OpportunityTickets">
@@ -17,14 +15,8 @@ const OpportunityTickets = () => {
           created opportunity tickets which will be free of charge.
         </p>
         <p>
-          To apply, please fill out this short
-          application <a href={formUrl} target="_blank" rel="noopener">form
-          </a> and tell us about your story so far. All information will be
-          kept confidential with the exception of the review committee.
-        </p>
-        <p>
-          Applications close: 13th January, 2017<br />
-          Application notification: 27th January, 2017
+          <strong>Applications close:</strong> 13th January, 2017<br />
+          <strong>Application notification:</strong> 27th January, 2017
         </p>
         <p>
           If you have any questions please email the team
