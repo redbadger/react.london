1a2f4e2fcfcdf519e106e9bc5038b3aa8d379ac2
Dominik Stodolny <dom@dmk.st>
2017-02-24 11:47:06 +0000

    add last commit date to the /version.txt

--- server/version.js
+++ server/version.js
@@ -1,6 +1,6 @@
 export default function versionTxt(req, res) {
-  const lastCommitSHA = process.env.GIT_COMMIT;
+  const lastCommitInfo = process.env.GIT_COMMIT;
   res.header('Content-Type', 'text/plain')
     .status(200)
-    .send(lastCommitSHA);
+    .send(lastCommitInfo);
 }
