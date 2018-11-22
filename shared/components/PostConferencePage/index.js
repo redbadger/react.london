46827ad90f3aa11087a5aff2ea9c3ec6d9da699f
Dominik Piatek <do.piatek@gmail.com>
2018-11-22 16:06:28 +0000

    Remove stay tuned section from conf site

--- shared/components/PostConferencePage/index.js
+++ shared/components/PostConferencePage/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-import MailingList from '../MailingList';
 import ConferenceAbout from '../ConferenceAbout';
 import ConferenceHighlights from '../ConferenceHightlights';
 
@@ -14,25 +13,20 @@ const PostConference = ({ finalStage }) => {
     <div>
       <Helmet
         meta={[
-           { name: 'twitter:card', content: 'summary_large_image' },
-           { name: 'twitter:site', content: '@ReactLondon_' },
-           { name: 'twitter:title', content: title },
-           { name: 'twitter:description', content: description },
-           { name: 'twitter:image', content: metaImage },
-           { property: 'og:type', content: 'website' },
-           { property: 'og:url', content: 'https://react.london' },
-           { property: 'og:title', content: title },
-           { property: 'og:image', content: metaImage },
-           { property: 'og:description', content: description },
+          { name: 'twitter:card', content: 'summary_large_image' },
+          { name: 'twitter:site', content: '@ReactLondon_' },
+          { name: 'twitter:title', content: title },
+          { name: 'twitter:description', content: description },
+          { name: 'twitter:image', content: metaImage },
+          { property: 'og:type', content: 'website' },
+          { property: 'og:url', content: 'https://react.london' },
+          { property: 'og:title', content: title },
+          { property: 'og:image', content: metaImage },
+          { property: 'og:description', content: description },
         ]}
       />
       <ConferenceAbout isAfterConference />
       <ConferenceHighlights finalStage={finalStage} />
-      <MailingList
-        mailingListTitle="Stay tuned"
-        mailingListSummary="Get post-conference emails and information on future events."
-        page="conference"
-      />
     </div>
   );
 };
